a4114a3d49eb5494ed52297870174052e20d84db
Sanjay Mijar <smijar@Ramyas-MacBook-12.local>
2018-06-22 12:01:17 +0530

    initial commit

---
+++ images/flash.js
@@ -0,0 +1,24 @@
+function gflash1()
+{
+		document.write('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="199" height="295" title="Gopalkrishna Temple, Mangalore">');
+		document.write(' <param name="movie" value="images/menu.swf">');
+        document.write('<param name="quality" value="high">');
+        document.write('<embed src="images/menu.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="199" height="295"></embed>');
+        document.write('</object>');
+}
+function gflash2()
+{
+		document.write('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="235" height="150">');
+		document.write('<param name="movie" value="images/PG.swf">');
+        document.write('<param name="quality" value="high">');
+        document.write('<embed src="images/PG.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="235" height="150"></embed>');
+        document.write('</object>');
+}
+function Photogallery()
+{
+		document.write('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="600" height="520">');
+		document.write('<param name="movie" value="images/Photogallery.swf">');
+        document.write('<param name="quality" value="high">');
+        document.write('<embed src="images/Photogallery.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="600" height="520"></embed>');
+        document.write('</object>');
+}
